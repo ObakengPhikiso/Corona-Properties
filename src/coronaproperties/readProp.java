b5a1eaef90a7025043245ed6e06fa3eefd398ffc
Melvin Kisten <iammelvink@gmail.com>
2020-07-22 21:33:08 +0200

    readProp method end

--- src/coronaproperties/readProp.java
+++ src/coronaproperties/readProp.java
@@ -1,8 +1,108 @@
 package coronaproperties;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  *
  * @author Melvin K
  */
 public class readProp {
+    private static class Property {
+        //fields
+        private String propertyPrimaryKey;
+        private String propertyType;
+        private String addressNum;
+        private String addressStreet;
+        private String addressCity;
+        private String addressCode;
+        private double value;
+        private boolean constructionComplete;
+        private String useOfProperty;
+        private int room;
+        private int garage;
+        private int bath;
+        private double floorArea;
+        private double landArea;
+        private double rates;
+        private String description;
+        private String telephone;
+        private String email;
+    }
+    private static Property propertyInfo;
+    readProp() {
+        displayData();
+    }
+
+    private static void displayData() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("/home/username/CoronaProperties.txt"));
+            String rec = reader.readLine();
+            String remain = "";
+
+            while (rec != null) {
+                propertyInfo = new Property();
+
+                propertyInfo.propertyPrimaryKey = rec.substring(0, rec.indexOf("#"));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.propertyType = remain.substring(0, remain.indexOf("#"));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.addressNum = remain.substring(0, remain.indexOf("#"));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.addressStreet = remain.substring(0, remain.indexOf("#"));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.addressCity = remain.substring(0, remain.indexOf("#"));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.addressCode = remain.substring(0, remain.indexOf("#"));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.value = Double.parseDouble(remain.substring(0, remain.indexOf("#")));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.constructionComplete = Boolean.parseBoolean(remain.substring(0, remain.indexOf("#")));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.useOfProperty = remain.substring(0, remain.indexOf("#"));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.room = Integer.parseInt(remain.substring(0, remain.indexOf("#")));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.garage = Integer.parseInt(remain.substring(0, remain.indexOf("#")));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.bath = Integer.parseInt(remain.substring(0, remain.indexOf("#")));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.floorArea = Double.parseDouble(remain.substring(0, remain.indexOf("#")));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.landArea = Double.parseDouble(remain.substring(0, remain.indexOf("#")));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.rates = Double.parseDouble(remain.substring(0, remain.indexOf("#")));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.description = remain.substring(0, remain.indexOf("#"));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.telephone = remain.substring(0, remain.indexOf("#"));
+                remain = remain.substring(remain.indexOf("#") + 1, remain.length());
+
+                propertyInfo.email = remain;
+                rec = reader.readLine();
+            }
+            reader.close();
+            System.out.println("\n\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
